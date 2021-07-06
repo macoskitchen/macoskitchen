95e3a6f507c8916523e0f8c194f69704c6c411a9
macoskitchen <86586685+macoskitchen@users.noreply.github.com>
2021-07-06 17:44:25 +0900

    JS:Work button

--- main.js
+++ main.js
@@ -52,6 +52,15 @@ arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 });
 
+// projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  console.log(filter);
+});
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: "smooth"});
